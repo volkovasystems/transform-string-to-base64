78fd7ece8b3d9f88d85118d3f0dcb5b73616e7f0
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-08-14 11:28:37 +0800

    Initial commit with transform-string-to-base64.js

---
+++ transform-string-to-base64.js
@@ -0,0 +1,68 @@
+/*:
+    @module-license:
+    The MIT License (MIT)
+
+    Copyright (c) 2014 Richeve Siodina Bebedor
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    @end-module-license
+
+    @module-configuration:
+        {
+            "packageName": "transform-base64-to-json",
+            "fileName": "transform-base64-to-json.js",
+            "moduleName": "transformBase64ToJSON",
+            "authorName": "Richeve S. Bebedor",
+            "authorEMail": "richeve.bebedor@gmail.com",
+            "repository": "git@github.com:volkovasystems/transform-base64-to-json.git",
+            "isGlobal": "true"
+        }
+    @end-module-configuration
+
+    @module-documentation:
+
+    @end-module-documentation
+
+    @include:
+    @end-include
+*/
+var transformBase64ToJSON = function transformBase64ToJSON( base64Data ){
+    /*:
+        @meta-configuration:
+            {
+                "base64Data:required": "string"
+            }
+        @end-meta-configuration
+    */
+
+    var buffer = new Buffer( base64Data, "base64" );
+
+    var JSONData = null;
+    try{
+        JSONData = JSON.parse( buffer.toString( ) );
+
+        return JSONData;
+
+    }catch( error ){
+        console.error( error );
+        throw error;
+    }
+};
+
+module.exports = transformBase64ToJSON;
